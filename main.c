2e9ce7f2b577d3a9c72a15c8cfbedbaa1a50f9e4
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 13:57:07 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "mamt.h"
 #include "merge_ranges.h"
 
+struct task
+{
+  int id;
+  double startT;
+  double endT;
+};
+
 int main(int argc, char* argv[]) {
+  //create array to save task data
+  
   //read in data
+  FILE *inFile, *outFile;
+  struct task input;
+  
+  inFile = fopen("task_data","r");
+  if (inFile == NULL) 
+  { 
+    fprintf(stderr, "File can't be opened.\n"); 
+    exit (1); 
+  } 
+  
+  outFile = fopen("task_data_output","w");
+  if (outFile == NULL) 
+  { 
+    fprintf(stderr, "File can't be opened.\n"); 
+    exit (1); 
+  } 
+
+  while(fread(&input, sizeof(struct task), 1, inFile)) 
+    //save contents to array
+  
+  fclose(inFile); 
   
   //call merge range func
   
@@ -19,6 +50,11 @@ int main(int argc, char* argv[]) {
   //sum valid records
   
   //write to csv
+  struct task output = {};
+  
+  fwrite(&output, sizeof(struct task), 1, outFile); 
+  
+  fclose(outFile);
   
   return 0;
 }
