69b72a7b5875ca5f16673dcd34a31d5ff43a1d04
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 18:39:29 -0500

    Update merge_ranges.c

--- merge_ranges.c
+++ merge_ranges.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
+#include "merge_ranges.h"
+
+struct interval
+{
+  double start;
+  double end;
+};
+
+int ascSort(const void* a, const void* b)
+{
+  return (*int(int* a) - *(int*)b );
+};
+
+double merge_ranges(){
+  
+  //sort array
+  qsort(inArr, n, sizeof(double), ascSort);
+  
+  return merge_arr;
+};
