7bf3699b52cb4b6a5ca2d5f6f01f38b4f1748478
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 11:26:15 -0500

    Update mamt.h

--- mamt.h
+++ mamt.h
@@ -1,7 +1,7 @@
 #ifndef MAMT_H
 #define MAMT_H
 
-// Returns the length of time it took for a task
-double mamt();
+// Returns the length of time it took for a task - mamt(active maintenance time)
+double* mamt(double* inArr, size_t n);
 
 #endif
