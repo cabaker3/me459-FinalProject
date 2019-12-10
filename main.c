bf75c55bc6042dd641f377f1713cd2e1c15347a4
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 13:31:38 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,18 @@
 #include "merge_ranges.h"
 
 int main(int argc, char* argv[]) {
+  //read in data
+  
+  //call merge range func
+  
+  //send merge range arr to mamt func
+  
+  //apply business rules - see valid records
+    //if mamt > 0 and < 16hrs, then 1 (count it as valid)
+    //else > 16hrs or < 0hrs, then 0 (not valid)
+  //sum valid records
+  
+  //write to csv
   
   return 0;
 }
