e569669fc6ce2e0d05198ffa38c5954d45317ad4
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 11:25:24 -0500

    Update mamt.c

--- mamt.c
+++ mamt.c
@@ -1,1 +1,20 @@
+#include <math.h>
+#include <stdlib.h>
 
+#include "mamt.h"
+
+#define HRS (24)
+
+double* mamt(double* inArr, size_t n){
+  //dynamically allocate arr
+  double* arr = malloc(sizeof(float) * (n/2));
+  double mamt;
+  
+  for(size_t i = 0; i < n; i++){
+    //calculate mamt (in hours)
+    mamt = abs(inArr[i] - inArr[i+1])*HRS;
+    arr[i] = mamt;
+  }
+  
+  return arr;
+}
