41f7a9dc9ac3c368f635ac003e5856877a886a70
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-10 09:21:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
 
+#include "mamt.h"
+#include "merge_ranges.h"
+
+int main(int argc, char* argv[]) {
+  
+  return 0;
+}
