849963e16e2420a2ad71bd995b81143e2c6f78e9
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-12 14:24:11 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mamt tool',
+    author='Caitlin Baker',
+    author_email='cabaker3@wisc.edu',
+    version="1.0.1",
+    packages = find_packages(),
+    description='Calculating maintenance task time',
+    install_requires=[
+        'pandas',
+    ]
+)
