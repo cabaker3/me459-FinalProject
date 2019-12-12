bde428ad239bb729156e3db70946fd3f1cb07676
Caitlin <31714143+cabaker3@users.noreply.github.com>
2019-12-12 16:04:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include "merge_ranges.h"
 #include "structs_def.h"
 
+#define S2MS (1000)
+
 int main(int argc, char* argv[]) {
   //read in data
   FILE *inFile, *outFile;
@@ -46,11 +48,17 @@ int main(int argc, char* argv[]) {
   fclose (inFile); 
   
   //call merge range func - get a new array of the time overlaps
+  clock_t start = clock();
   double* arr = merge_ranges(input, sizeof(input)/sizeof(*input));
+  clock_t end = clock();
+  double time = ((double)(end - start)) / CLOCKS_PER_SEC * S2MS;
   
   //convert struct back to regular array
   //send merge range arr to mamt func - get the total task time
+  clock_t start1 = clock();
   double mamt_total = mamt(arr, vals);
+  clock_t end1 = clock();
+  double time1 = ((double)(end1 - start1)) / CLOCKS_PER_SEC * S2MS;
   
   //write to csv
   //struct task output = {};
